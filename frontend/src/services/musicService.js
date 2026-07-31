c333229298abd8067dab0103ed3a7edb2bf53f1b
Taimo <chickenvn003@gmail..com>
2026-07-31 13:43:58 +0700

    update

--- frontend/src/services/musicService.js
+++ frontend/src/services/musicService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = import.meta.env.VITE_API_URL ?? '/api';
+const baseURL = import.meta.env.VITE_API_URL ?? 'https://l2f6sx8v-4000.asse.devtunnels.ms/api';
 
 const api = axios.create({
   baseURL,
